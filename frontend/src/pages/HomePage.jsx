6f24bdf5c534c0b5b7fa9a8e45617e8664edc640
Raj Soni <rajksoni029@gmail.com>
2024-04-01 23:17:30 +0530

    react + vite frontend added

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+        <img src="/src/assets/logo.png" className="logo" alt="Vite logo" />
+        <h1>Multi Music</h1>
+    </>
+  )
+}
+
+export default HomePage
